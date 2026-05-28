5ebe84d5e3b2da1ed3c6dd250a73df6630b08f4a
kiyo99 <kiofavour@gmail.com>
2026-05-28 15:28:16 -0400

    Initial Commit

---
+++ FoodMart/FoodMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMartApp.swift
+//  FoodMart
+//
+//  Created by Godsfavour Ngo Kio on 2026-05-28.
+//
+
+import SwiftUI
+
+@main
+struct FoodMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodMartUITests/FoodMartUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  FoodMartUITestsLaunchTests.swift
+//  FoodMartUITests
+//
+//  Created by Godsfavour Ngo Kio on 2026-05-28.
+//
+
+import XCTest
+
+final class FoodMartUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
